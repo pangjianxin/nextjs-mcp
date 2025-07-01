f4d56159fab8dbb9f3fe34dcd0a3421d64347f35
pangjianxin <343516704@qq.com>
2025-07-01 18:08:31 +0800

    c

--- server/src/Wallee.Mcp.EntityFrameworkCore/Migrations/20250701094833_MigrateTo921.Designer.cs
+++ server/src/Wallee.Mcp.EntityFrameworkCore/Migrations/20250701094833_MigrateTo921.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Wallee.Mcp.EntityFrameworkCore;
 namespace Wallee.Mcp.Migrations
 {
     [DbContext(typeof(McpDbContext))]
-    partial class McpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250701094833_MigrateTo921")]
+    partial class MigrateTo921
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/src/Wallee.Mcp.EntityFrameworkCore/Migrations/20250701094833_MigrateTo921.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wallee.Mcp.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateTo921 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationName",
+                table: "AbpBackgroundJobs",
+                type: "character varying(96)",
+                maxLength: 96,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationName",
+                table: "AbpBackgroundJobs");
+        }
+    }
+}
