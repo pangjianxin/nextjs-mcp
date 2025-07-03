7fb24ccd4060ca5c9850d2482e2cac537eace9c9
pangjianxin <343516704@qq.com>
2025-07-03 21:11:24 +0800

    c

--- server/src/Wallee.Mcp.Agents/McpAgentsModule.cs
+++ server/src/Wallee.Mcp.Agents/McpAgentsModule.cs
@@ -2,10 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Agents;
-using Microsoft.SemanticKernel.Agents.Orchestration.GroupChat;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using System;
-using System.Threading.Tasks;
 using Volo.Abp.Modularity;
 using Wallee.Mcp.Services;
 
@@ -13,6 +11,7 @@ namespace Wallee.Mcp.Agents
 {
     public class McpAgentsModule : AbpModule
     {
+        
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             var configuration = context.Services.GetConfiguration();
@@ -46,25 +45,8 @@ namespace Wallee.Mcp.Agents
                         ServiceId = "deepseek-chat",
                         FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
                     })
-                };
-
+                };            
                 //agent.Kernel.Plugins.AddFromObject();
-
-
-#pragma warning disable SKEXP0110 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
-                var groupChat = new GroupChatOrchestration(new RoundRobinGroupChatManager(), agent)
-                {
-                    ResponseCallback = (response) =>
-                     {
-                         // Handle the response from the agent
-#pragma warning disable SKEXP0001 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
-                         Console.WriteLine($"Response from agent encoding: {response.Encoding},{response.AuthorName}");
-#pragma warning restore SKEXP0001 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
-                         return ValueTask.CompletedTask;
-                     },
-                };
-#pragma warning restore SKEXP0110 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
-
                 return agent;
             });
         }
