5982be4b1bd6f87b991375b62a26d58672534571
pangjianxin <343516704@qq.com>
2025-07-08 19:29:33 +0800

    'c'

--- server/src/Wallee.Mcp.Servers/McpServersModule.cs
+++ server/src/Wallee.Mcp.Servers/McpServersModule.cs
@@ -7,20 +7,17 @@ using Volo.Abp.Modularity;
 
 namespace Wallee.Mcp
 {
-    [DependsOn(typeof(McpDomainModule))]
+    [DependsOn(typeof(McpDomainModule), typeof(McpApplicationContractsModule))]
     public class McpServersModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            context.Services.AddKeyedTransient("QiXin", (sp, _) =>
+            var configuration = context.Services.GetConfiguration();
+
+            context.Services.AddHttpClient("TianYanCha", (serviceProvider, client) =>
             {
-                var configuration = sp.GetRequiredService<IConfiguration>();
-                var apiKey = configuration["QiXin:ApiKey"];
-                var apiSecret = configuration["QiXin:ApiSecret"];
-                var client = new HttpClient() { BaseAddress = new Uri("https://api.weather.gov") };
-                client.DefaultRequestHeaders.Add("", "2d024790-38f8-4a4e-8c76-75f499db4c1e");
-                client.DefaultRequestHeaders.Add("", "cf35421a-7963-437c-8cd1-316fa7f9cc3c");
-                return client;
+                client.DefaultRequestHeaders.Add("Authorization", configuration[$"{OpenRemoteServiceConsts.TianYanCha}:Token"]);
+                client.BaseAddress = new Uri(configuration[$"{OpenRemoteServiceConsts.TianYanCha}:BaseUrl"]!);
             });
         }
     }
